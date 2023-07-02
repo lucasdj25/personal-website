7f3aa522c16fce32709d5fb348cc56e8f75e2552
lucasdj25 <lucasjmax@gmail.com>
2023-07-02 15:09:25 -0400

    Adds different sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import Home from './components/sections/Home/Home';
+import About from './components/sections/About/About';
+import Resume from './components/sections/Resume/Resume';
+import Projects from './components/sections/Projects/Projects';
+import Contact from './components/sections/Contact/Contact';
 
 function App() {
   return (
     <div className="App">
-
+      <Home />
+      <About />
+      <Resume />
+      <Projects />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/sections/About/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <div className="about-section section">
+            <h1>About</h1>
+        </div>
+    );
+}
+
+export default About;
