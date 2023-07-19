6922d007f7ea5d4b7cb17770a5cb04ea45cb1fab
Lucas Johnson <johnsluc@mail.gvsu.edu>
2023-07-19 16:56:56 -0400

    removes unneeded package

---
+++ src/components/Common/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./ProjectCard.css"
+
+function ProjectCard(){
+
+    return (
+        <div className='projectCard'>
+            BRO LOOK AT DIS PROJECT
+        </div>
+    )
+}
+
+export default ProjectCard
