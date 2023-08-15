f7c7def0af010ce2820699e8b55164f1cdedaa08
Lucas Johnson <johnsluc@mail.gvsu.edu>
2023-08-14 22:57:55 -0400

    Removes Section container styling

--- src/components/sections/Resume/Resume.tsx
+++ src/components/sections/Resume/Resume.tsx
@@ -12,7 +12,7 @@ function Resume() {
             <div className='centered'>
                 <SectionIntro title='Resume' desc='My work and education experience.' light={true} />
                 <a className='button' target="_blank" href={require("../../../assets/files/resume.pdf")} rel="noreferrer">Download CV</a>
-                <div className='section-main-container grey rounded-corners'>
+                <div className='section-main-container'>
                     <div className='work-section'>
                         <h4>Work</h4>
                         {resumeWorkInfo.map((item, i) => {
