b1ee649f043724ad6ec7bb8e08d4d62481a3dca2
lucasdj25 <lucasjmax@gmail.com>
2023-08-14 21:04:27 -0400

    Even more styling changes

--- src/components/Common/ProjectModal/ProjectModal.tsx
+++ src/components/Common/ProjectModal/ProjectModal.tsx
@@ -4,45 +4,47 @@ import "./ProjectModal.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTag } from '@fortawesome/free-solid-svg-icons'
 
-interface ProjectModalProps{
+interface ProjectModalProps {
     setModalActive: React.Dispatch<React.SetStateAction<boolean>>
     project: ProjectModel | undefined
 }
 
-export default function ProjectModal({setModalActive, project}: ProjectModalProps) {
+export default function ProjectModal({ setModalActive, project }: ProjectModalProps) {
 
     const [smallScreen, setSmallScreen] = useState(window.innerWidth <= 1000 ? true : false)
 
-    function handleResize(){
-        if(window.innerWidth <= 1000){
+    function handleResize() {
+        if (window.innerWidth <= 1000) {
             setSmallScreen(true)
-        }else{
+        } else {
             setSmallScreen(false)
         }
     }
 
-    useEffect(()=> {
+    useEffect(() => {
         window.addEventListener("resize", handleResize)
 
         return () => {
             window.removeEventListener("resize", handleResize)
         }
     }, [])
-        
+
     return (
         <>
-            <span className="modal-background" onClick={() => { setModalActive(false) } } />
+            <span className="modal-background" onClick={() => { setModalActive(false) }} />
             <div className='project-modal'>
                 <div className='project-modal-grid-container'>
-                    <div className='modal-image' style={{backgroundImage: `url(${project?.image})`}}></div>
+                    <div className='modal-image' style={{ backgroundImage: `url(${project?.image})` }}></div>
                     <div className='modal-content'>
                         <table>
-                            <tbody>
+                            <thead>
                                 {!smallScreen && (
-                                    <tr>
-                                        <td><span className='exit-button' onClick={()=>setModalActive(false)}>X</span></td>
+                                    <tr >
+                                        <th><span className='exit-button' onClick={() => setModalActive(false)}>X</span></th>
                                     </tr>
                                 )}
+                            </thead>
+                            <tbody>
                                 <tr className='space-under'>
                                     <td><h1>{project?.title}</h1></td>
                                 </tr>
@@ -52,8 +54,8 @@ export default function ProjectModal({setModalActive, project}: ProjectModalProp
                                 {(project && project.skills.length > 0) && (
                                     <tr className='space-above '>
                                         <td className='project-modal-skills'>
-                                            <FontAwesomeIcon icon={faTag} className='tag-icon' />    
-                                            {project.skills.reduce((skillString, skill)=> {
+                                            <FontAwesomeIcon icon={faTag} className='tag-icon' />
+                                            {project.skills.reduce((skillString, skill) => {
                                                 return `${skillString}, ${skill}`
                                             })}
                                         </td>
@@ -65,10 +67,10 @@ export default function ProjectModal({setModalActive, project}: ProjectModalProp
                 </div>
                 {(project?.links || smallScreen) && (<div className='project-modal-footer'>
                     <ul>
-                        {project?.links && project.links.map((p, i)=> {
+                        {project?.links && project.links.map((p, i) => {
                             return <li key={`projectlink${i}`}><a href={p.url} className='footer-button' target='_blank' rel="noreferrer">{p.name}</a></li>
                         })}
-                        {smallScreen && <li className='footer-button' onClick={()=>setModalActive(false)}>Close</li>}
+                        {smallScreen && <li className='footer-button' onClick={() => setModalActive(false)}>Close</li>}
                     </ul>
                 </div>)}
             </div>
--- src/components/sections/About/About.tsx
+++ src/components/sections/About/About.tsx
@@ -10,11 +10,11 @@ function About() {
         <div id="about-section" className="section content-section">
             <div className='centered'>
                 <SectionIntro title='About' desc='This is me.' light={false} />
-                <div className='section-main-container dark rounded-corners'>
+                <div className='section-main-container rounded-corners'>
                     <div className='introduction'>
                         <div className='picture-div' />
                         <p>Hello, I'm Lucas Johnson! I am a software developer who recently graduated from Grand Valley State University with a Bachelor's degree in Computer science
-                            and I'm looking to further grow my technical and soft skills. I am extremely passionate about web development, however I am always eager to learn about new branches of 
+                            and I'm looking to further grow my technical and soft skills. I am extremely passionate about web development, however I am always eager to learn about new branches of
                             software development whenever possible! Feel free to contact me if you would like to learn more about me.</p>
                     </div>
                     <a className='button' href={"#contact-section"}>Contact me</a>
